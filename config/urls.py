270ecf5661214f34d9d60415e6d3ca57f60879c3
SAPR <ivancko.max@gmail.com>
2026-02-04 15:02:56 +0700

    hgjgjhhgjhg

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,10 @@ from django.urls import path
 # Импортируем нашу функцию из приложения gallery
 from gallery.views import home
 urlpatterns = [
-path('admin/', admin.site.urls, 'about/'),
-# Пустая строка '' означает главную страницу сайта (http://localhost:8000/)
-path('', home, name='home', about, name="about"),
+    path('admin/', admin.site.urls),
+    # Пустая строка '' означает главную страницу сайта (http://localhost:8000/)
+    path('', home, name='home'),
+
+    path('about/', about, name='about'),
+
 ]
