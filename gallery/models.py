2bab8a2d5ff41a49e25cc3f6fe0d2586989998f7
Kaenair <mi.ladygin@gmail.com>
2026-02-09 19:19:37 +0700

    3lab

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+class Asset(models.Model):
+    # Поле для названия (строка до 200 символов)
+    title = models.CharField(max_length=200, verbose_name="Название модели")
 
-# Create your models here.
+
+    file = models.FileField(upload_to='3d_assets/', verbose_name="3D Файл")
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата загрузки")
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = "3D Модель"
+        verbose_name_plural = "3D Модели"
--- gallery/views.py
+++ gallery/views.py
@@ -1,27 +1,20 @@
 from django.shortcuts import render
+from .models import Asset
 
 # Create your views here.
 from django.http import HttpResponse
 
 def home(request):
-    # 1. Готовим данные (Context). Это словарь Python.
-    # Ключи словаря станут именами переменных в HTML.
+
     context_data = {
     'page_title': 'Главная Галерея',
     'models_count': 5, # Попробуйте поменять на 5, чтобы проверить условие
     }
-    # 2. Рендерим (смешиваем HTML и данные)
-    # Путь указываем относительно папки templates: 'gallery/index.html'
-    return render(request, 'gallery/index.html', context_data)
-    # Имитация данных из базы (список словарей)
-    fake_database = [
-        {'id': 1, 'name': 'Sci-Fi Helmet', 'file_size': '15 MB'},
-        {'id': 2, 'name': 'Old Chair', 'file_size': '2 MB'},
-        {'id': 3, 'name': 'Cyber Truck', 'file_size': '10 MB'},
-    ]
+
+    assets = Asset.objects.all()
     context_data = {
         'page_title': 'Главная Галерея',
-        'assets': fake_database, # Передаем весь список
+        'assets': assets, # Передаем реальный QuerySet (список)
     }
     return render(request, 'gallery/index.html', context_data)
 
