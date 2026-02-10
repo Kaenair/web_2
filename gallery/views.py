788cf88fd24459bf16a64cce9dc78fe471fec603
Kaenair <mi.ladygin@gmail.com>
2026-02-10 22:47:25 +0700

    Сортировка на главной

--- gallery/views.py
+++ gallery/views.py
@@ -8,13 +8,13 @@ def home(request):
 
     context_data = {
     'page_title': 'Главная Галерея',
-    'models_count': 5, # Попробуйте поменять на 5, чтобы проверить условие
+    'models_count': 5, 
     }
 
-    assets = Asset.objects.all()
+    assets = Asset.objects.all().order_by('-created_at')
     context_data = {
         'page_title': 'Главная Галерея',
-        'assets': assets, # Передаем реальный QuerySet (список)
+        'assets': assets,
     }
     return render(request, 'gallery/index.html', context_data)
 
